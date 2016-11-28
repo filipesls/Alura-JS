e9db9c72b0717602182054ced5b7f337a054bdff
Filipe da Silva <fdasilva@interactiveservices.com>
2016-11-28 16:32:41 +0000

    DOM e objetos

--- JAVASCRIPT/projeto/calcula-imc.js
+++ JAVASCRIPT/projeto/calcula-imc.js
@@ -1,9 +1,19 @@
 //calcula-imc.js
-var peso = 100;
-var altura = 0;
 
-if(altura != 0){
-    var imc = peso / (altura * altura);
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var paciente = {peso : peso, altura : altura};
+
+if(paciente.altura != 0){
+    var imc = paciente.peso / (paciente.altura * paciente.altura);
+
+	var tdImc = document.getElementById("imc-2"); 
+	tdImc.textContent = imc;
+
     console.log(imc);
 } else{
     console.log("Não posso executar uma divisão por 0!");
