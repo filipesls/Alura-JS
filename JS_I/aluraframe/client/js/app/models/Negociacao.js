f494c29576585e4c4aa339c73a367835b2593c12
Filipe Santana <filipe.santana@gmail.com>
2018-03-24 00:51:03 +0000

    Aula 2

---
+++ JS_I/aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,27 @@
+//Criando uma classe
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+        Object.freeze(this);
+    }
+    
+    get data() {
+        return this._data;
+    }
+    
+    get quantidade() {
+        return this._quantidade;
+    }
+    
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
